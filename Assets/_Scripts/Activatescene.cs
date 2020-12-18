6ea410d71c80a2ea00b30b40b6e7986bb0dd1a52
Jin3314 <56128184+Jin3314@users.noreply.github.com>
2020-12-18 20:23:51 +0900

    Assets
    Assets

---
+++ Assets/_Scripts/Activatescene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activatescene : MonoBehaviour
+{
+
+    bool IsPause;
+
+    // Use this for initialization
+    void Start()
+    {
+        IsPause = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            
+            if (IsPause == false)
+            {
+                Time.timeScale = 0;
+                IsPause = true;
+                return;
+            }
+
+           
+            if (IsPause == true)
+            {
+                Time.timeScale = 1;
+                IsPause = false;
+                return;
+            }
+        }
+    }
+}
+
+
